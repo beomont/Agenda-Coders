99452a17ce2baccc1c6400d503d82196f9aaf6cb
Batcleta <35137108+Batcleta@users.noreply.github.com>
2022-12-12 22:47:13 -0300

    feat: adicionando inputs string nullable & notNullable

--- src/main/java/org/agenda/utils/Input.java
+++ src/main/java/org/agenda/utils/Input.java
@@ -7,4 +7,40 @@ public class Input {
         return new Scanner(System.in).nextInt();
     }
 
+    public static String string(String nomeDoCampo) {
+        System.out.printf("Por favor informe o %s\n", nomeDoCampo);
+        return new Scanner(System.in).nextLine().trim();
+    }
+
+    public static String stringNotNullable(String nomeDoCampo, int tentativas) throws Exception {
+        int tentadas = 0;
+        boolean isInvalid;
+        String input = "";
+
+        do {
+            isInvalid = false;
+
+            try {
+                input = string(nomeDoCampo);
+
+                if (input.isBlank()) throw new Exception(nomeDoCampo);
+
+
+            } catch (Exception ex) {
+                tentadas += 1;
+
+                System.out.printf("msg: %s, não pode ser nulo\n", ex.getMessage());
+                if (tentadas < tentativas) {
+                    isInvalid = true;
+                    continue;
+                }
+
+                throw new Exception("Número de tentativas excedidas");
+            }
+
+        } while (isInvalid);
+
+        return input;
+    }
+
 }
